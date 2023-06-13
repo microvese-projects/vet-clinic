9bd2e8cd8fc90e250346ca4f719bfafb569ccec9
Benson Njuguna <njugunab655@gmail.com>
2023-06-13 09:23:59 +0300

    Select animals with a given weight limit including the limits

--- queries.sql
+++ queries.sql
@@ -7,3 +7,4 @@ SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered=true;
 SELECT * FROM animals WHERE name='Gabumon';
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
