34787d32688b928c17de08ec4bf772939cb912ff
Benson Njuguna <njugunab655@gmail.com>
2023-06-13 09:13:05 +0300

    Query name

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' and '2019-12-31';
-SELECT * FROM animals WHERE neutered=true AND escape_attempts < 3;
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' and '2019-12-31';
+SELECT name FROM animals WHERE neutered=true AND escape_attempts < 3;
 
