b7f7667e40bef81efc716aac25d1ec3aa6915b83
Benson Njuguna <njugunab655@gmail.com>
2023-06-13 18:46:04 +0300

    Add transactions

--- queries.sql
+++ queries.sql
@@ -8,3 +8,12 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered=true;
 SELECT * FROM animals WHERE name!='Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+-- Transactions
+BEGIN;
+DELETE FROM animals WHERE date_of_birth >= '2022-01-01';
+SAVEPOINT SVP1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SVP1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
