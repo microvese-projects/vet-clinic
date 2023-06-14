0d69ca6b37a97337b1cc9e03f45e994223fb3b33
Benson Njuguna <njugunab655@gmail.com>
2023-06-14 12:53:03 +0300

    Alter animals table

--- data.sql
+++ data.sql
@@ -18,8 +18,3 @@ VALUES
   ('Boarmon', '2005-06-07', 7, true, 20.4),
   ('Blossom', '1998-08-13', 3, true, 17),
   ('Ditto', '2022-05-14', 4, true, 22);
-
--- drop species
-BEGIN;
-ALTER TABLE animals DROP species;
-COMMIT;
