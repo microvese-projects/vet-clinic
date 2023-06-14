e5d6d113a6872b00af606e06251380177e0b3034
Benson Njuguna <njugunab655@gmail.com>
2023-06-14 14:07:26 +0300

    What animals belong to Melody Pond

--- queries.sql
+++ queries.sql
@@ -46,3 +46,10 @@ SELECT species, AVG(escape_attempts)
 FROM animals
 WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
 GROUP BY species;
+
+-- What animals belong to Melody Pond?
+SELECT name AS AnimalName
+FROM animals
+JOIN owners
+ON animals.owner_id = owners.id
+WHERE owners.full_name = 'Melody Pond';
